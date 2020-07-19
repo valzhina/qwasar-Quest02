1c608e8f6e3d4dfaa6cb229df8063999274cd14f
olya piskun <piskun_o@qwasar.io>
2020-07-19 06:57:45 +0000

    ex05

---
+++ ex05/my_sub.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int my_sub(int param_1, int param_2)
+{
+	int substr;
+
+	substr = param_1 - param_2;
+	return (substr);	
+}
+/*
+int	main() {
+
+	int a = 0;
+	int b = 1;
+
+	int c = 10;
+    	int d = 10;
+
+	int e = -10;
+    	int f = 10;
+
+	printf("a-b=%d\n",my_sub(a, b));
+	printf("c-d=%d\n",my_sub(c, d));
+	printf("e-f=%d\n",my_sub(e, f));
+	return (0);
+}
+*/
