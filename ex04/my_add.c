11366a6d28f1615cfba7ae2e336d437f8416c8f9
olya piskun <piskun_o@qwasar.io>
2020-07-19 06:51:19 +0000

    ex04

---
+++ ex04/my_add.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int my_add(int param_1, int param_2) {
+	int sum;
+
+	sum = param_1 + param_2;
+	return (sum);
+}
+/*
+int	main() {
+	
+	int a = 0;
+	int b = 1;
+
+	int c = 10;
+    int d = 10;
+
+	int e = -10;
+    int f = 10;
+
+	printf("a+b=%d\n",my_add(a, b));
+	printf("c+d=%d\n",my_add(c, d));
+	printf("e+f=%d\n",my_add(e, f));
+	return (0);
+}
+*/
