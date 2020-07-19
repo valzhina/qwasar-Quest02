d702a13144c9a8b05332ea71906eb419f32a5115
olya piskun <piskun_o@qwasar.io>
2020-07-19 07:02:54 +0000

    ex06

---
+++ ex06/my_mult.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int my_mult(int param_1, int param_2)
+{
+	int mu;
+
+	mu = param_1 * param_2;
+	return (mu);	
+}
+/*
+int	main() {
+
+	int a = 0;
+	int b = 1;
+
+	int c = 10;
+    int d = 10;
+
+	int e = -10;
+    int f = 10;
+
+	printf("a*b=%d\n",my_mult(a, b));
+	printf("c*d=%d\n",my_mult(c, d));
+	printf("e*f=%d\n",my_mult(e, f));
+	return (0);
+}
+*/
+
